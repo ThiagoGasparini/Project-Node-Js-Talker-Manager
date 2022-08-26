3c57710250eb689d32ec270772d446a8b9a91418
Thiago Gasparini <thiago_engprod@outlook.com>
2022-08-26 16:32:14 -0300

    req 2

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -13,4 +13,18 @@ router.get('/', async (_req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const talkers = await readContentFile();
+    const users = talkers.find((user) => user.id === Number(id));
+    if (!users) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' }); 
+    }
+    return res.status(200).json(users);
+  } catch (err) {
+   console.log(err);
+  }
+});
+
 module.exports = router;
